09261b4618f99ca2294feb3d5b0d87f547c9a619
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-10-15 11:01:00 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(GetChar("abcd", "ceadb"));
+        Console.WriteLine(GetChar("abbdd", "dabadb"));
+        Console.WriteLine(GetChar("", "y"));
+        Console.WriteLine(GetChar("aaaa", "aabaa"));
+        Console.WriteLine(GetChar("ae", "aabaa"));
+    }
+
+    internal static char GetChar(string s, string t)
+    {
+        int sizeS = s.Length;
+        int sizeT = t.Length;
+        for (int i = 0; i < sizeS; i++)
+        {
+            for (int j = 0; j < sizeT; j++)
+            {
+                if (s[i] == t[j])
+                {
+                    t = t.Remove(j, 1);
+                    sizeT--;
+                    break;
+                }
+            }
+        }
+        return t[0];
+    }
+}
